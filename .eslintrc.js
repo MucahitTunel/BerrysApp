f67946d6fd39f707d91e8fea338118134d5dc97a
Nguyen Xuan Thai <thainx@dgroup.co>
2019-10-17 13:52:41 +0700

    add import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community', 'prettier'],
+  plugins: ['import'],
   rules: {
     'no-await-in-loop': 2,
     'import/no-unresolved': 'off',
   },
+  settings: {
+    "import/resolver": {
+      "babel-module": {}
+    }
+  },  
 }
