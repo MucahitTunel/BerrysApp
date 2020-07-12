aaf0b1cdda3d133dafe03d7b5a63f8a2ce7b7c82
Son Le <sonlt@dgroup.co>
2020-07-13 00:20:11 +0700

    add services/contacts

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,9 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const signIn = createAsyncThunk(
+  'users/signIn',
+  async ({ phoneNumber, password, countryCode }) => {},
+)
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {},
+  initialState: {
+    user: null,
+    points: 0,
+  },
   reducers: {},
+  extraReducers: {
+    [signIn.fulfilled]: (state, action) => {},
+  },
 })
 
 export const {
---
+++ src/services/contacts/helpers.js
@@ -0,0 +1,87 @@
+import { Platform, PermissionsAndroid } from 'react-native'
+import Contacts from 'react-native-contacts'
+import forEach from 'lodash/forEach'
+import uniqueId from 'lodash/uniqueId'
+import DeviceInfo from 'react-native-device-info'
+import { parsePhoneNumber } from 'libphonenumber-js/max'
+
+export const formatPhoneNumber = (phone, countryCode) => {
+  const DEVICE_COUNTRY_FALLBACK = 'US'
+  try {
+    let userLocaleCountryCode = DeviceInfo.getDeviceCountry()
+    if (!userLocaleCountryCode) {
+      userLocaleCountryCode = countryCode || DEVICE_COUNTRY_FALLBACK
+    }
+    const phoneNumber = parsePhoneNumber(
+      `Phone: ${phone}`,
+      userLocaleCountryCode,
+    )
+    return {
+      number: phoneNumber.number,
+      isValid: phoneNumber.isValid(),
+    }
+  } catch (error) {
+    console.log('ERROR - formatPhoneNumber')
+    console.log(error)
+    return {
+      number: phone,
+      isValid: false,
+    }
+  }
+}
+
+export const getPhoneBookContacts = () =>
+  new Promise((resolve, reject) => {
+    if (Platform.OS === 'ios') {
+      Contacts.getAll((err, contacts) => {
+        if (!err) {
+          resolve(contacts)
+        } else {
+          reject(err)
+        }
+      })
+    }
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+        title: 'Contacts',
+        message: 'This app would like to view your contacts',
+      }).then(() => {
+        Contacts.getAll((err, contacts) => {
+          if (!err) {
+            resolve(contacts)
+          } else {
+            reject(err)
+          }
+        })
+      })
+    }
+  })
+
+export const formatContacts = (phoneBookContacts) => {
+  const contacts = []
+  forEach(phoneBookContacts, (contact) => {
+    forEach(contact.phoneNumbers, (phoneObject) => {
+      if (phoneObject) {
+        const { number, isValid } = formatPhoneNumber(phoneObject.number)
+        if (number && isValid) {
+          let name = `${contact.givenName} ${contact.familyName}`
+          // Remove white spaces
+          name = name.trim()
+          if (name && name.length) {
+            if (number) {
+              if (!contacts.find((c) => c.phoneNumber === number)) {
+                const contactObject = {
+                  _id: uniqueId('temp_'),
+                  name,
+                  phoneNumber: number,
+                }
+                contacts.push(contactObject)
+              }
+            }
+          }
+        }
+      }
+    })
+  })
+  return contacts
+}
