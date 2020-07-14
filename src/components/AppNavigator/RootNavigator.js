188624e4d9782d9180e7acc231b449c21c4ba8bf
Son Le <sonlt@dgroup.co>
2020-07-14 19:15:49 +0700

    remove headerTransparent option

--- src/components/AppNavigator/RootNavigator.js
+++ src/components/AppNavigator/RootNavigator.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import Config from 'react-native-config'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Header } from 'components'
 import { MenuButton, MessagesButton } from 'components/NavButton'
-import StorybookButton from 'components/AppButton/StorybookButton'
 import Constants from 'constants'
 import Onboarding from 'features/auth/Onboarding'
 import Splash from 'features/auth/Splash'
 import Main from 'features/questions/Main'
 
 const Stack = createStackNavigator()
-
-const storyBookNavigator = ['dev', 'staging'].includes(Config.ENV)
-  ? {
-      headerTransparent: true,
-      headerTitle: null,
-      headerRight: () => <StorybookButton isHidden={true} />,
-    }
-  : { header: () => null }
 
 const RootNavigator = () => {
   const auth = useSelector((state) => state.auth)
@@ -27,7 +17,7 @@ const RootNavigator = () => {
     return <Splash />
   }
   return (
-    <Stack.Navigator screenOptions={storyBookNavigator}>
+    <Stack.Navigator>
       {user ? (
         <>
           <Stack.Screen
