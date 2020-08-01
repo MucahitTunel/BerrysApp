a69cfc80a6685306ba5b230e6319acd08efcb6d3
Son Le <sonlt@dgroup.co>
2020-08-01 10:22:55 +0700

    update get-ip script

--- scripts/get-ip.sh
+++ scripts/get-ip.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # We need to get the IPv4 version so that Android emulator can connect to the server
-ip_pattern='192\.168\.[0-9]*\.[0-9]*'
-host=`ifconfig | grep -oh $ip_pattern | head -1`
+host=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 server_port=3000
 # Update HOST
 sed -i '' "s|.*API_URL=.*|API_URL=http://$host:$server_port/|" ./fastlane/.env.dev
