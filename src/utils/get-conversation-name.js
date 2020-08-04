ec12550eff8d9b07f931dc8a9e270d0862789964
Son Le <sonlt@dgroup.co>
2020-08-05 01:01:26 +0700

    modify conversation desc logic

--- src/utils/get-conversation-name.js
+++ src/utils/get-conversation-name.js
@@ -1,6 +1,14 @@
 import store from 'state/store'
 
+const getNumberFromCreatedAt = (phoneNumber, createdAt) => {
+  const milli = String(new Date(createdAt).getTime())
+  return milli.substr(phoneNumber.length - 3)
+}
+
 export const getConversationName = (room) => {
+  const notAnonymous =
+    "You're not anonymous. Your friend wants to talk specifically with you"
+  const anonymous = "You're anonymous and your identity won't be revealed"
   const { data: customData, createdBy, members, createdAt } = room
   const state = store.getState()
   const {
@@ -11,26 +19,38 @@ export const getConversationName = (room) => {
   const otherUserPhoneNumber = members.find((m) => m !== user.phoneNumber)
   const isRoomCreator = phoneNumber === createdBy
   const isFromQuestionPage = customData && customData.isFromQuestionPage
-  const shouldShowAnonymous = !isRoomCreator || isFromQuestionPage
-  if (shouldShowAnonymous) {
-    const milli = String(new Date(createdAt).getTime())
-    const number = milli.substr(phoneNumber.length - 3)
+  const number = getNumberFromCreatedAt(phoneNumber, createdAt)
+  if (isFromQuestionPage) {
+    // conversation starts from Question page
     return {
       title: `Anonymous ${number}`,
-      description: "You're anonymous and your identity won't be revealed",
+      description: anonymous,
     }
-  }
-  if (data && data.length) {
-    const contact = data.find((c) => c.phoneNumber === otherUserPhoneNumber)
-    if (contact && contact.name) {
+  } else {
+    // conversation starts by 1 user (roomCreator)
+    if (isRoomCreator) {
+      // this is the room creator
+      if (data && data.length) {
+        const contact = data.find((c) => c.phoneNumber === otherUserPhoneNumber)
+        if (contact && contact.name) {
+          return {
+            title: contact.name,
+            description: anonymous,
+          }
+        }
+      }
+    } else {
+      // not a room creator
       return {
-        title: contact.name,
-        description:
-          "You're not anonymous. Your friend wants to talk specifically with you",
+        title: `Anonymous ${number}`,
+        description: notAnonymous,
       }
     }
   }
-  return otherUserPhoneNumber
+  return {
+    title: otherUserPhoneNumber,
+    description: '',
+  }
 }
 
 export default getConversationName
