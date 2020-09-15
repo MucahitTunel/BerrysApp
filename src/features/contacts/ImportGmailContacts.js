2a42d2fbead2f7a804ab1fca57bdba3cee2fbc7a
Son Le <sonlt@dgroup.co>
2020-09-15 21:36:57 +0700

    login with google on iOS

--- src/features/contacts/ImportGmailContacts.js
+++ src/features/contacts/ImportGmailContacts.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import { StatusBar, StyleSheet, View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { StatusBar, StyleSheet, View } from 'react-native'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin'
+import Config from 'react-native-config'
 import Constants from 'constants'
 
 const styles = StyleSheet.create({
@@ -12,10 +18,34 @@ const styles = StyleSheet.create({
 })
 
 const ImportGmailContacts = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/contacts.readonly'],
+      webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+    })
+  })
+  const onPressLoginWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      console.log('userInfo')
+      console.log(userInfo)
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other errors happened
+      }
+    }
+  }
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <Text>Import Gmail Contacts</Text>
+      <GoogleSigninButton onPress={onPressLoginWithGoogle} />
     </View>
   )
 }
