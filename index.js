4cd4cacef0843514497c4ea40779ceaed2a0016f
Son Le <sonlt@dgroup.co>
2020-11-11 13:13:33 +0700

    do not show some warnings

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import { LogBox } from 'react-native'
 
 AppRegistry.registerComponent(appName, () => App)
+LogBox.ignoreLogs([
+  'Animated: `useNativeDriver` was not specified',
+  'Warning: componentWillMount has been renamed',
+  'Deprecation warning: use moment.updateLocale',
+  'Warning: componentWillReceiveProps',
+])
