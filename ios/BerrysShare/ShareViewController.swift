0d3546c40aee63484eae6ff8f331ab496ae76bba
Son Le <sonlt@dgroup.co>
2020-11-14 11:23:02 +0700

    update fastlane

--- ios/BerrysShare/ShareViewController.swift
+++ ios/BerrysShare/ShareViewController.swift
@@ -15,7 +15,7 @@ import Photos
 
 class ShareViewController: SLComposeServiceViewController {
   // TODO: IMPORTANT: This should be your host app bundle identifier
-let hostAppBundleIdentifier = "com.berrys.nickapp.dev"
+let hostAppBundleIdentifier = "com.berrys.nickapp"
   let sharedKey = "ShareKey"
   var sharedMedia: [SharedMediaFile] = []
   var sharedText: [String] = []
